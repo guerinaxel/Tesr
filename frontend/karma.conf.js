41cf6406e451aa217e574b23d5b6914f28683895
Ha1One <guerinaxel57@gmail.com>
2025-11-21 08:39:50 +0100

    Add test result publishing to CI workflows

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
@@ -21,7 +22,12 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
-    reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+      outputDir: 'test-results',
+      outputFile: 'karma-results.xml',
+      useBrowserName: false,
+    },
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
